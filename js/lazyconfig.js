4b70d3eaf726eed3d8ced3c336a8ca2a3c1b89ca
Leander Wernst <leanderwernst@msn.com>
2023-01-21 18:37:15 +0100

    add lazyloading to images via 'lazysizes'

---
+++ js/lazyconfig.js
@@ -0,0 +1,5 @@
+window.lazySizesConfig = window.lazySizesConfig || {};
+window.lazySizesConfig.lazyClass = 'lazyload';
+lazySizesConfig.srcAttr = 'data-src';
+lazySizesConfig.loadMode = 1;
+lazySizesConfig.loadHidden = false;
